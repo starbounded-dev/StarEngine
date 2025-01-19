0c6a4838fd34e74ff9e5c772c34f64abfad7e63e
sheazy_wi <sheazywi@gmail.com>
2025-01-19 11:35:46 -0500

    added windows events
    windowclose, windowresize, key events, mouse events, and print them as trace

--- StarStudio/src/StarStudio/Application.cpp
+++ StarStudio/src/StarStudio/Application.cpp
@@ -1,20 +1,30 @@
 #include "sspch.h"
 #include "Application.h"
 
-#include "StarStudio/Events/ApplicationEvent.h"
 #include "StarStudio/Log.h"
 
 #include <GLFW/glfw3.h>
 
 namespace StarStudio
+
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
 {
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application()
 	{
+	}
+
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		SS_CORE_TRACE("{0}", e);
 	}
 
 	void Application::Run()
@@ -25,6 +35,12 @@ namespace StarStudio
 			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
 	}
 }
 
--- StarStudio/src/StarStudio/Application.h
+++ StarStudio/src/StarStudio/Application.h
@@ -2,6 +2,7 @@
 
 #include "Core.h"
 #include "Events/Event.h"
+#include "Events/ApplicationEvent.h"
 
 #include "Window.h"
 
@@ -14,7 +15,11 @@ namespace StarStudio
 			virtual ~Application();
 
 			void Run();
+
+			void OnEvent(Event& e);
 		private:
+			bool OnWindowClose(WindowCloseEvent& e);
+
 			std::unique_ptr<Window> m_Window;
 			bool m_Running = true;
 	};
