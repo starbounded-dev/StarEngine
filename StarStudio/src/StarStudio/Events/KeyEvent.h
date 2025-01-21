49d54f020ff8213cdec5d1a14825759885ae4955
sheazy_wi <sheazywi@gmail.com>
2025-01-20 20:35:42 -0500

    made imgui events and fixed something from the original imgui repo

--- StarStudio/src/StarStudio/Events/KeyEvent.h
+++ StarStudio/src/StarStudio/Events/KeyEvent.h
@@ -45,4 +45,19 @@ namespace StarStudio {
 		}
 		EVENT_CLASS_TYPE(KeyReleased)
 	};
+
+	class STARSTUDIO_API KeyTypedEvent : public KeyEvent
+	{
+	public:
+		KeyTypedEvent(int keycode)
+			: KeyEvent(keycode) {
+		}
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "KeyTypedEvent: " << m_KeyCode;
+			return ss.str();
+		}
+		EVENT_CLASS_TYPE(KeyTyped)
+	};
 }
--- StarStudio/src/StarStudio/ImGui/ImGuiLayer.h
+++ StarStudio/src/StarStudio/ImGui/ImGuiLayer.h
@@ -2,6 +2,10 @@
 
 #include "StarStudio/Layer.h"
 
+#include "StarStudio/Events/ApplicationEvent.h"
+#include "StarStudio/Events/KeyEvent.h"
+#include "StarStudio/Events/MouseEvent.h"
+
 namespace StarStudio
 {
 	class STARSTUDIO_API ImGuiLayer : public Layer
@@ -14,6 +18,15 @@ namespace StarStudio
 		void OnDetach();
 		void OnUpdate();
 		void OnEvent(Event& event);
+	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizeEvent(WindowResizeEvent& e);
 	private:
 		float m_Time = 0.0f;
 	};
