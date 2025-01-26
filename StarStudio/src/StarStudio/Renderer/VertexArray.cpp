05334b35d3d51e3840c7bcd708048079d1c39f5b
sheazy_wi <sheazywi@gmail.com>
2025-01-26 14:10:40 -0500

    added vertex array

---
+++ StarStudio/src/StarStudio/Renderer/VertexArray.cpp
@@ -0,0 +1,20 @@
+#include "sspch.h"
+#include "VertexArray.h"
+
+#include "Renderer.h"
+#include "Platform/OpenGL/OpenGLVertexArray.h"
+
+namespace StarStudio {
+
+	VertexArray* VertexArray::Create() {
+
+		switch (Renderer::GetAPI())
+		{
+			case RendererAPI::None:	SS_CORE_ASSERT(false, "RendererAPI::None is currently not supported") return nullptr;
+			case RendererAPI::OpenGL:	return new OpenGLVertexArray();
+		}
+
+		SS_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}
+}
