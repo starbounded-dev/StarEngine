86f95d13b5ad3b39e682e7884e28f4650a6c25da
sheazy_wi <sheazywi@gmail.com>
2025-02-01 17:42:44 -0500

    preparing the area

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -159,7 +159,7 @@ class ExampleLayer : public StarStudio::Layer
 			std::dynamic_pointer_cast<StarStudio::OpenGLShader>(m_FlatColorShader)->Bind();
 			std::dynamic_pointer_cast<StarStudio::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat3("u_Color", m_SquareColor);
 
-			for (int y = 0; y < 20; y++)
+			/*for (int y = 0; y < 20; y++)
 			{
 				for (int x = 0; x < 20; x++)
 				{
@@ -168,9 +168,12 @@ class ExampleLayer : public StarStudio::Layer
 
 					StarStudio::Renderer::Submit(m_FlatColorShader, m_SquareVA, transform);
 				}
-			}
+			}*/
 
-			StarStudio::Renderer::Submit(m_Shader, m_VertexArray);
+			StarStudio::Renderer::Submit(m_FlatColorShader, m_SquareVA, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));
+
+			//triangle
+			//StarStudio::Renderer::Submit(m_Shader, m_VertexArray);
 
 			StarStudio::Renderer::EndScene();
 		}
