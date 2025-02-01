98d1770200f1f4620dc1bb6a34cf768a02ce4216
sheazy_wi <sheazywi@gmail.com>
2025-02-01 18:46:46 -0500

    OpenGLVertexArray not adding multible VertexBuffers correctly

--- StarStudio/src/Platform/OpenGL/OpenGLVertexArray.cpp
+++ StarStudio/src/Platform/OpenGL/OpenGLVertexArray.cpp
@@ -52,8 +52,8 @@ namespace StarStudio {
 		const auto& layout = vertexBuffer->GetLayout();
 		for (const auto& element : layout)
 		{
-			glEnableVertexAttribArray(index);
-			glVertexAttribPointer(index,
+			glEnableVertexAttribArray(index + m_VertexBufferIndexOffset);
+			glVertexAttribPointer(index + m_VertexBufferIndexOffset,
 				element.GetComponentCount(),
 				ShaderDataTypeToOpenGLBaseType(element.Type),
 				element.Normalized ? GL_TRUE : GL_FALSE,
@@ -63,6 +63,7 @@ namespace StarStudio {
 		}
 
 		m_VertexBuffers.push_back(vertexBuffer);
+		m_VertexBufferIndexOffset += layout.GetElements().size();
 	}
 
 	void OpenGLVertexArray::SetIndexBuffer(const Ref<IndexBuffer>& indexBuffer) {
