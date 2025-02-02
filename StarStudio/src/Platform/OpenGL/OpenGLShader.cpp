ae4b04b6fcb4de1f54fea43f1e63dcdad58cecfe
sheazy_wi <sheazywi@gmail.com>
2025-02-02 08:47:41 -0500

    fixed size issue

--- StarStudio/src/Platform/OpenGL/OpenGLShader.cpp
+++ StarStudio/src/Platform/OpenGL/OpenGLShader.cpp
@@ -85,7 +85,8 @@ namespace StarStudio {
 	void OpenGLShader::Compile(const std::unordered_map<GLenum, std::string>& shaderSources)
 	{
 		GLuint program = glCreateProgram();
-		std::vector<GLenum> glShaderIDs(shaderSources.size());
+		std::vector<GLenum> glShaderIDs;
+		glShaderIDs.reserve(shaderSources.size());
 		for (auto& kv : shaderSources)
 		{
 			GLenum type = kv.first;
