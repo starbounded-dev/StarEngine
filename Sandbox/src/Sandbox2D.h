0a728cf3f99367f59b209656ccd97eda85dbba0d
sheazy_wi <sheazywi@gmail.com>
2025-02-04 20:23:17 -0500

    profiling

--- Sandbox/src/Sandbox2D.h
+++ Sandbox/src/Sandbox2D.h
@@ -25,5 +25,13 @@ private:
 
 	StarStudio::Ref<StarStudio::Texture2D> m_CheckerboardTexture;
 
+	struct ProfileResult
+	{
+		const char* Name;
+		float Time;
+	};
+
+	std::vector<ProfileResult> m_ProfileResults;
+
 	glm::vec4 m_SquareColor = { 0.2f, 0.3f, 0.8f, 1.0f };
 };
--- StarStudio/src/StarStudio/ImGui/ImGuiLayer.cpp
+++ StarStudio/src/StarStudio/ImGui/ImGuiLayer.cpp
@@ -85,10 +85,4 @@ namespace StarStudio {
 		}
 	}
 
-	void ImGuiLayer::OnImGuiRender()
-	{
-		static bool show = true;
-		ImGui::ShowDemoWindow(&show);
-	}
-
 }
