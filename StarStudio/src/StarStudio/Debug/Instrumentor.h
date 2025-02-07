6e88f04bf9afe21e2f1b221f55a6ca5d01ffd9c3
sheazy_wi <sheazywi@gmail.com>
2025-02-06 19:14:20 -0500

    Fixed Instrumentor generating same start time

--- StarStudio/src/StarStudio/Debug/Instrumentor.h
+++ StarStudio/src/StarStudio/Debug/Instrumentor.h
@@ -3,16 +3,20 @@
 #include <algorithm>
 #include <chrono>
 #include <fstream>
-
+#include <iomanip>
 #include <string>
 #include <thread>
 
 namespace StarStudio {
 
+	using FloatingPointMicroseconds = std::chrono::duration<double, std::micro>;
+
 	struct ProfileResult
 	{
 		std::string Name;
-		long long Start, End;
+
+		FloatingPointMicroseconds Start;
+		std::chrono::microseconds ElapsedTime;
 		std::thread::id ThreadID;
 	};
 
@@ -23,29 +27,31 @@ namespace StarStudio {
 
 	class Instrumentor
 	{
-
 	private:
 		std::mutex m_Mutex;
 		InstrumentationSession* m_CurrentSession;
 		std::ofstream m_OutputStream;
-
 	public:
 		Instrumentor()
 			: m_CurrentSession(nullptr)
 		{
-
 		}
 
 		void BeginSession(const std::string& name, const std::string& filepath = "results.json")
 		{
 			std::lock_guard lock(m_Mutex);
 			if (m_CurrentSession) {
+				// If there is already a current session, then close it before beginning new one.
+				// Subsequent profiling output meant for the original session will end up in the
+				// newly opened session instead.  That's better than having badly formatted
+				// profiling output.
 				if (Log::GetCoreLogger()) { // Edge case: BeginSession() might be before Log::Init()
 					SS_CORE_ERROR("Instrumentor::BeginSession('{0}') when session '{1}' already open.", name, m_CurrentSession->Name);
 				}
 				InternalEndSession();
 			}
 			m_OutputStream.open(filepath);
+
 			if (m_OutputStream.is_open()) {
 				m_CurrentSession = new InstrumentationSession({ name });
 				WriteHeader();
@@ -70,17 +76,18 @@ namespace StarStudio {
 			std::string name = result.Name;
 			std::replace(name.begin(), name.end(), '"', '\'');
 
+			json << std::setprecision(3) << std::fixed;
 			json << ",{";
 			json << "\"cat\":\"function\",";
-			json << "\"dur\":" << (result.End - result.Start) << ',';
+			json << "\"dur\":" << (result.ElapsedTime.count()) << ',';
 			json << "\"name\":\"" << name << "\",";
 			json << "\"ph\":\"X\",";
 			json << "\"pid\":0,";
 			json << "\"tid\":" << result.ThreadID << ",";
-			json << "\"ts\":" << result.Start;
+			json << "\"ts\":" << result.Start.count();
 			json << "}";
-			std::lock_guard lock(m_Mutex);
 
+			std::lock_guard lock(m_Mutex);
 			if (m_CurrentSession) {
 				m_OutputStream << json.str();
 				m_OutputStream.flush();
@@ -106,6 +113,8 @@ namespace StarStudio {
 			m_OutputStream.flush();
 		}
 
+		// Note: you must already own lock on m_Mutex before
+		// calling InternalEndSession()
 		void InternalEndSession() {
 			if (m_CurrentSession) {
 				WriteFooter();
@@ -123,7 +132,7 @@ namespace StarStudio {
 		InstrumentationTimer(const char* name)
 			: m_Name(name), m_Stopped(false)
 		{
-			m_StartTimepoint = std::chrono::high_resolution_clock::now();
+			m_StartTimepoint = std::chrono::steady_clock::now();
 		}
 
 		~InstrumentationTimer()
@@ -134,45 +143,44 @@ namespace StarStudio {
 
 		void Stop()
 		{
-			auto endTimepoint = std::chrono::high_resolution_clock::now();
+			auto endTimepoint = std::chrono::steady_clock::now();
+			auto highResStart = FloatingPointMicroseconds{ m_StartTimepoint.time_since_epoch() };
+			auto elapsedTime = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch() - std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimepoint).time_since_epoch();
 
-			long long start = std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimepoint).time_since_epoch().count();
-			long long end = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch().count();
-
-			Instrumentor::Get().WriteProfile({ m_Name, start, end, std::this_thread::get_id() });
+			Instrumentor::Get().WriteProfile({ m_Name, highResStart, elapsedTime, std::this_thread::get_id() });
 
 			m_Stopped = true;
 		}
-
 	private:
 		const char* m_Name;
-		std::chrono::time_point<std::chrono::high_resolution_clock> m_StartTimepoint;
+		std::chrono::time_point<std::chrono::steady_clock> m_StartTimepoint;
 		bool m_Stopped;
 	};
 }
 
 #define SS_PROFILE 1
 #if SS_PROFILE
-	// Resolve which function signature macro will be used. Note that this only
-	// is resolved when the (pre)compiler starts, so the syntax highlighting
-	// could mark the wrong one in your editor!
-	#if defined(__GNUC__) || (defined(__MWERKS__) && (__MWERKS__ >= 0x3000)) || (defined(__ICC) && (__ICC >= 600)) || defined(__ghs__)
-	#define SS_FUNC_SIG __PRETTY_FUNCTION__
-	#elif defined(__DMC__) && (__DMC__ >= 0x810)
-	#define SS_FUNC_SIG __PRETTY_FUNCTION__
-	#elif defined(__FUNCSIG__)
-	#define SS_FUNC_SIG __FUNCSIG__
-	#elif (defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 600)) || (defined(__IBMCPP__) && (__IBMCPP__ >= 500))
-	#define SS_FUNC_SIG __FUNCTION__
-	#elif defined(__BORLANDC__) && (__BORLANDC__ >= 0x550)
-	#define SS_FUNC_SIG __FUNC__
-	#elif defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901)
-	#define SS_FUNC_SIG __func__
-	#elif defined(__cplusplus) && (__cplusplus >= 201103)
-	#define SS_FUNC_SIG __func__
-	#else
-	#define HZ_FUNC_SIG "HZ_FUNC_SIG unknown!"
-	#endif
+// Resolve which function signature macro will be used. Note that this only
+// is resolved when the (pre)compiler starts, so the syntax highlighting
+// could mark the wrong one in your editor!
+#if defined(__GNUC__) || (defined(__MWERKS__) && (__MWERKS__ >= 0x3000)) || (defined(__ICC) && (__ICC >= 600)) || defined(__ghs__)
+#define SS_FUNC_SIG __PRETTY_FUNCTION__
+#elif defined(__DMC__) && (__DMC__ >= 0x810)
+#define SS_FUNC_SIG __PRETTY_FUNCTION__
+#elif defined(__FUNCSIG__)
+#define SS_FUNC_SIG __FUNCSIG__
+#elif (defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 600)) || (defined(__IBMCPP__) && (__IBMCPP__ >= 500))
+#define SS_FUNC_SIG __FUNCTION__
+#elif defined(__BORLANDC__) && (__BORLANDC__ >= 0x550)
+#define SS_FUNC_SIG __FUNC__
+#elif defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901)
+#define SS_FUNC_SIG __func__
+#elif defined(__cplusplus) && (__cplusplus >= 201103)
+#define SS_FUNC_SIG __func__
+#else
+#define SS_FUNC_SIG "SS_FUNC_SIG unknown!"
+#endif
+
 #define SS_PROFILE_BEGIN_SESSION(name, filepath) ::StarStudio::Instrumentor::Get().BeginSession(name, filepath)
 #define SS_PROFILE_END_SESSION() ::StarStudio::Instrumentor::Get().EndSession()
 #define SS_PROFILE_SCOPE(name) ::StarStudio::InstrumentationTimer timer##__LINE__(name);
