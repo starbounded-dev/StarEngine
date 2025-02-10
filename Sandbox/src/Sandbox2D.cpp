51a525b3470abb59420d19cac28b4e613e017885
sheazy_wi <sheazywi@gmail.com>
2025-02-09 19:57:46 -0500

    Renderer2D can now render rotated quads in batches + we now use transforms

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -39,13 +39,16 @@ void Sandbox2D::OnUpdate(StarStudio::Timestep ts)
 	}
 
 	{
-		SS_PROFILE_SCOPE("Renderer2D::BeginScene");
+		static float rotation = 0.0f;
+		rotation += ts * 50.0f;
+
+		SS_PROFILE_SCOPE("Renderer Draw");
 		StarStudio::Renderer2D::BeginScene(m_CameraController.GetCamera());
-		// StarStudio::Renderer2D::DrawRotatedQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, glm::radians(-45.0f), { 0.8f, 0.2f, 0.3f, 1.0f });
+		StarStudio::Renderer2D::DrawRotatedQuad({ 1.0f, 0.0f }, { 0.8f, 0.8f }, -45.0f, { 0.8f, 0.2f, 0.3f, 1.0f });
 		StarStudio::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
 		StarStudio::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
-		StarStudio::Renderer2D::DrawQuad({ -5.0f, -5.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture, 10.0f);
-		StarStudio::Renderer2D::DrawQuad({ -0.5f, -0.5f, 0.0f }, { 1.0f, 1.0f }, m_CheckerboardTexture, 20.0f);
+		StarStudio::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture, 10.0f);
+		StarStudio::Renderer2D::DrawRotatedQuad({ -2.0f, 0.0f, 0.0f }, { 1.0f, 1.0f }, rotation, m_CheckerboardTexture, 20.0f);
 		StarStudio::Renderer2D::EndScene();
 	}
 }
