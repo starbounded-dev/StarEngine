3fa1afe6dc8d169d4c36f84fa9a727e8533dc419
sheazy_wi <sheazywi@gmail.com>
2025-02-15 16:28:15 -0500

    added particle system

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -18,6 +18,14 @@ void Sandbox2D::OnAttach()
 	SE_PROFILE_FUNCTION();
 
 	m_CheckerboardTexture = StarEngine::Texture2D::Create("assets/textures/Checkerboard.png");
+
+	m_Particle.ColorBegin = { 254 / 255.0f, 212 / 255.0f, 123 / 255.0f, 1.0f };
+	m_Particle.ColorEnd = { 254 / 255.0f, 109 / 255.0f, 41 / 255.0f, 1.0f };
+	m_Particle.SizeBegin = 0.5f, m_Particle.SizeVariation = 0.3f, m_Particle.SizeEnd = 0.0f;
+	m_Particle.LifeTime = 0.6f;
+	m_Particle.Velocity = { 0.0f, 0.0f };
+	m_Particle.VelocityVariation = { 3.0f, 1.0f };
+	m_Particle.Position = { 0.0f, 0.0f };
 }
 
 void Sandbox2D::OnDetach()
@@ -38,19 +46,20 @@ void Sandbox2D::OnUpdate(StarEngine::Timestep ts)
 		StarEngine::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
 		StarEngine::RenderCommand::Clear();
 	}
-
+	
 	{
 		static float rotation = 0.0f;
 		rotation += ts * 50.0f;
 
 		SE_PROFILE_SCOPE("Renderer Draw");
 		StarEngine::Renderer2D::BeginScene(m_CameraController.GetCamera());
-		StarEngine::Renderer2D::DrawRotatedQuad({ 1.0f, 0.0f }, { 0.8f, 0.8f }, -45.0f, { 0.8f, 0.2f, 0.3f, 1.0f });
+		StarEngine::Renderer2D::DrawRotatedQuad({ 1.0f, 0.0f }, { 0.8f, 0.8f }, glm::radians(-45.0f), { 0.8f, 0.2f, 0.3f, 1.0f });
 		StarEngine::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
 		StarEngine::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, m_SquareColor);
 		StarEngine::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 20.0f, 20.0f }, m_CheckerboardTexture, 10.0f);
-		StarEngine::Renderer2D::DrawRotatedQuad({ -2.0f, 0.0f, 0.0f }, { 1.0f, 1.0f }, rotation, m_CheckerboardTexture, 20.0f);
+		StarEngine::Renderer2D::DrawRotatedQuad({ -2.0f, 0.0f, 0.0f }, { 1.0f, 1.0f }, glm::radians(rotation), m_CheckerboardTexture, 20.0f);
 		StarEngine::Renderer2D::EndScene();
+
 
 		StarEngine::Renderer2D::BeginScene(m_CameraController.GetCamera());
 		for (float y = -5.0f; y < 5.0f; y += 0.5f)
@@ -63,6 +72,24 @@ void Sandbox2D::OnUpdate(StarEngine::Timestep ts)
 		}
 		StarEngine::Renderer2D::EndScene();
 	}
+	
+	if (StarEngine::Input::IsMouseButtonPressed(SE_MOUSE_BUTTON_LEFT))
+	{
+		auto [x, y] = StarEngine::Input::GetMousePosition();
+		auto width = StarEngine::Application::Get().GetWindow().GetWidth();
+		auto height = StarEngine::Application::Get().GetWindow().GetHeight();
+
+		auto bounds = m_CameraController.GetBounds();
+		auto pos = m_CameraController.GetCamera().GetPosition();
+		x = (x / width) * bounds.GetWidth() - bounds.GetWidth() * 0.5f;
+		y = bounds.GetHeight() * 0.5f - (y / height) * bounds.GetHeight();
+		m_Particle.Position = { x + pos.x, y + pos.y };
+		for (int i = 0; i < 5; i++)
+			m_ParticleSystem.Emit(m_Particle);
+	}
+
+	m_ParticleSystem.OnUpdate(ts);
+	m_ParticleSystem.OnRender(m_CameraController.GetCamera());
 }
 
 void Sandbox2D::OnImGuiRender()
@@ -72,17 +99,12 @@ void Sandbox2D::OnImGuiRender()
 	ImGuiContext& g = *GImGui;
 	ImGuiIO& io = g.IO;
 
-	#ifdef SE_DIST
-	ImGui::Begin("Distribution Build");
-	ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
-	if (ImGui::Checkbox("VSync", &m_VSync)) {
-		StarEngine::Application::Get().GetWindow().SetVSync(m_VSync);
-	}
-	ImGui::End();
-	#endif
+#ifdef SE_DIST
+	SE_CORE_INFO("FPS : {0}", io.Framerate);
+#endif
 
 
-	#ifdef SE_DEBUG
+#ifdef SE_DEBUG
 
 	//Camera Info
 	ImGui::Begin("Camera Info");
@@ -98,7 +120,7 @@ void Sandbox2D::OnImGuiRender()
 		m_CameraController.GetCamera().SetPosition(glm::vec3(0.0f));
 		m_CameraController.GetCamera().SetRotation(0.0f);
 	}
-	
+
 	//Color Picker
 	ImGui::Text("Color Picker");
 
@@ -122,7 +144,13 @@ void Sandbox2D::OnImGuiRender()
 
 	ImGui::End();
 
-	#endif
+#endif
+
+	ImGui::Begin("Settings");
+	ImGui::ColorEdit4("Birth Color", glm::value_ptr(m_Particle.ColorBegin));
+	ImGui::ColorEdit4("Death Color", glm::value_ptr(m_Particle.ColorEnd));
+	ImGui::DragFloat("Life Time", &m_Particle.LifeTime, 0.1f, 0.0f, 1000.0f);
+	ImGui::End();
 }
 
 void Sandbox2D::OnEvent(StarEngine::Event& e)
--- StarEngine/src/StarEngine/Renderer/OrthographicCameraController.h
+++ StarEngine/src/StarEngine/Renderer/OrthographicCameraController.h
@@ -8,6 +8,15 @@
 
 namespace StarEngine {
 
+	struct OrthographicCameraBounds
+	{
+		float Left, Right;
+		float Bottom, Top;
+
+		float GetWidth() { return Right - Left; }
+		float GetHeight() { return Top - Bottom; }
+	};
+
 	class OrthographicCameraController
 	{
 
@@ -22,21 +31,21 @@ namespace StarEngine {
 
 		float GetZoomLevel() const { return m_ZoomLevel; }
 		void SetZoomLevel(float level) { m_ZoomLevel = level; }
+
+		const OrthographicCameraBounds& GetBounds() const { return m_Bounds; }
 	private:
 		bool OnMouseScrolled(MouseScrolledEvent& e);
 		bool OnWindowResized(WindowResizeEvent& e);
 	private:
 		float m_AspectRatio;
 		float m_ZoomLevel = 1.0f;
-
+		OrthographicCameraBounds m_Bounds;
 		OrthographicCamera m_Camera;
 
 		bool m_Rotation;
 
 		glm::vec3 m_CameraPosition = { 0.0f, 0.0f, 0.0f };
-
 		float m_CameraRotation = 0.0f;
-
 		float m_CameraTranslationSpeed = 5.0f, m_CameraRotationSpeed = 90.0f;
 	};
 }
