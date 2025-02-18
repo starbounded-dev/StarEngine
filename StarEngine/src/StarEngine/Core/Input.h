f49f25af1b1881387beb70d31a508154a528bd6b
Pier-Olivier Boulianne <sheazywi@starbounded.me>
2025-02-17 20:04:37 -0500

    destructors

--- StarEngine/src/StarEngine/Core/Input.h
+++ StarEngine/src/StarEngine/Core/Input.h
@@ -11,6 +11,8 @@ namespace StarEngine {
 	protected:
 		Input() = default;
 	public:
+		virtual ~Input() = default;
+
 		Input(const Input&) = delete;
 		Input& operator=(const Input&) = delete;
 
--- StarEngine/src/StarEngine/Renderer/Framebuffer.h
+++ StarEngine/src/StarEngine/Renderer/Framebuffer.h
@@ -14,6 +14,7 @@ namespace StarEngine {
 	class Framebuffer
 	{
 	public:
+		virtual ~Framebuffer() = default;
 		virtual void Bind() = 0;
 		virtual void Unbind() = 0;
 
