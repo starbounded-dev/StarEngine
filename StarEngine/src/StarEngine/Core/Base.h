b993f14a010316fda7190bd469dfcbcdcdf186d0
Pier-Olivier Boulianne <sheazywi@starbounded.me>
2025-02-20 22:18:59 -0500

    replaced std::bind in SE_BIND_EVENT_FN macro

--- StarEngine/src/StarEngine/Core/Base.h
+++ StarEngine/src/StarEngine/Core/Base.h
@@ -69,7 +69,7 @@
 
 #define BIT(x) (1 << x)
 
-#define SE_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
+#define SE_BIND_EVENT_FN(fn) [this](auto&&... args) -> decltype(auto) { return this->fn(std::forward<decltype(args)>(args)...); }
 
 namespace StarEngine {
 
