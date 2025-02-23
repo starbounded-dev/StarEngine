a13e0ef3984f1b038781d22905624e3151e4b349
Pier-Olivier Boulianne <sheazywi@starbounded.me>
2025-02-22 21:32:06 -0500

    Add EventCategoryMouseButton to MouseButtonEvent

--- StarEngine/src/StarEngine/Events/MouseEvent.h
+++ StarEngine/src/StarEngine/Events/MouseEvent.h
@@ -56,7 +56,7 @@ namespace StarEngine {
 	public:
 		MouseCode GetMouseButton() const { return m_Button; }
 
-		EVENT_CLASS_CATEGORY(EventCategoryMouse | EventCategoryInput)
+		EVENT_CLASS_CATEGORY(EventCategoryMouse | EventCategoryInput | EventCategoryMouseButton)
 	protected:
 		MouseButtonEvent(const MouseCode button)
 			: m_Button(button) {
