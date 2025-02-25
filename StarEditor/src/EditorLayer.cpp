9c43a6927b584f417c74a245df15b6cf8625265a
Pier-Olivier Boulianne <sheazywi@starbounded.me>
2025-02-24 21:24:35 -0500

    Complete remaster of StarEditor's look and few tweaks
    - Added fonts

--- StarEditor/src/EditorLayer.cpp
+++ StarEditor/src/EditorLayer.cpp
@@ -160,12 +160,19 @@ namespace StarEngine {
 		if (opt_fullscreen)
 			ImGui::PopStyleVar(2);
 
-		// Submit the DockSpace
+
+		// DockSpace
+		ImGuiStyle& style = ImGui::GetStyle();
+		float minWinSizeX = style.WindowMinSize.x;
+		style.WindowMinSize.x = 370.0f;
+
 		if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
 		{
 			ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
 			ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
 		}
+
+		style.WindowMinSize.x = minWinSizeX;
 
 		if (ImGui::BeginMenuBar())
 		{
