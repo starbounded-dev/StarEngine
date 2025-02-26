49ec75ada73d96c95a3ac93727ec7ca51089dfd3
Pier-Olivier Boulianne <sheazywi@starbounded.me>
2025-02-26 18:31:05 -0500

    Prevent crash when user adds a component that is already presen

--- StarEditor/src/Panels/SceneHierarchyPanel.cpp
+++ StarEditor/src/Panels/SceneHierarchyPanel.cpp
@@ -233,13 +233,19 @@ namespace StarEngine{
 		{
 			if (ImGui::MenuItem("Camera"))
 			{
-				m_SelectionContext.AddComponent<CameraComponent>();
+				if (!m_SelectionContext.HasComponent<CameraComponent>())
+					m_SelectionContext.AddComponent<CameraComponent>();
+				else
+					SE_CORE_WARN("This entity already has the Camera Component!");
 				ImGui::CloseCurrentPopup();
 			}
 
 			if (ImGui::MenuItem("Sprite Renderer"))
 			{
-				m_SelectionContext.AddComponent<SpriteRendererComponent>();
+				if (!m_SelectionContext.HasComponent<SpriteRendererComponent>())
+					m_SelectionContext.AddComponent<SpriteRendererComponent>();
+				else
+					SE_CORE_WARN("This entity already has the Sprite Renderer Component!");
 				ImGui::CloseCurrentPopup();
 			}
 
