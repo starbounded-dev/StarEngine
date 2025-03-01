00ed4259b947a733737fc4e528a5e7ae3d1b82bb
Pier-Olivier Boulianne <sheazywi@starbounded.me>
2025-02-28 23:34:59 -0500

    Fixed Math function declaration

--- StarEngine/src/StarEngine/Math/Math.h
+++ StarEngine/src/StarEngine/Math/Math.h
@@ -3,5 +3,5 @@
 #include <glm/glm.hpp>
 
 namespace StarEngine::Math {
-	bool DecomposeTransform(const glm::mat4& transform, glm::vec3& outTranslation, glm::vec3& outRotation, glm::vec3& outScale);
+	bool DecomposeTransform(const glm::mat4& transform, glm::vec3& translation, glm::vec3& rotation, glm::vec3& scale);
 }
