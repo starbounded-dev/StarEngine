0071fb38f67269b9252e21560cc4de0bdb64effa
Pier-Olivier Boulianne <sheazywi@starbounded.me>
2025-02-28 23:00:51 -0500

    i feel stupid

--- StarEngine/src/StarEngine/Scene/Components.h
+++ StarEngine/src/StarEngine/Scene/Components.h
@@ -38,7 +38,7 @@ namespace StarEngine {
 
 		glm::mat4 GetTransform() const
 		{
-			glm::mat4 rotation = glm::toMat4(glm::quat(rotation));
+			glm::mat4 rotation = glm::toMat4(glm::quat(Rotation));
 
 			return glm::translate(glm::mat4(1.0f), Translation)
 				* rotation
