53b31ec26d2f1a15b1089ab7887e624f6fa47ec5
Pier-Olivier Boulianne <sheazywi@starbounded.me>
2025-03-10 20:55:17 -0400

    Added missing 'SetFloat2' in Renderer for completeness

--- StarEngine/src/Platform/OpenGL/OpenGLShader.cpp
+++ StarEngine/src/Platform/OpenGL/OpenGLShader.cpp
@@ -213,6 +213,12 @@ namespace StarEngine {
 		UploadUniformFloat(name, value);
 	}
 
+	void OpenGLShader::SetFloat2(const std::string& name, const glm::vec2& value)
+	{
+		SE_PROFILE_FUNCTION();
+		UploadUniformFloat2(name, value);
+	}
+
 	void OpenGLShader::SetFloat3(const std::string& name, const glm::vec3& value)
 	{
 		SE_PROFILE_FUNCTION();
