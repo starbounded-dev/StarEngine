69430aaffd151fd7fb3cd6af0c13c522f97c7b29
Pier-Olivier Boulianne <sheazywi@starbounded.me>
2025-03-11 21:32:21 -0400

    Update premake5.lua

--- StarEngine/vendor/GLAD/premake5.lua
+++ StarEngine/vendor/GLAD/premake5.lua
@@ -1,49 +1,30 @@
-project "GLAD"
-	kind "StaticLib"
-	language "C"
-	staticruntime "on"
-	warnings "off"
+project "Glad"
+    kind "StaticLib"
+    language "C"
+    staticruntime "on"
+    
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    files
+    {
+        "include/glad/glad.h",
+        "include/KHR/khrplatform.h",
+        "src/glad.c"
+    }
 
-	files
-	{
-		"include/glad/glad.h",
-		"include/KHR/khrplatform.h",
-		"src/glad.c"
-	}
+    includedirs
+    {
+        "include"
+    }
+    
+    filter "system:windows"
+        systemversion "latest"
 
-	includedirs
-	{
-		"include"
-	}
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
 
-	filter "system:linux"
-		pic "On"
-		systemversion "latest"
-
-	filter "system:macosx"
-		pic "On"
-
-	filter "system:windows"
-		systemversion "latest"
-
-	filter "configurations:Debug"
-		runtime "Debug"
-		symbols "on"
-
-	filter { "system:windows", "configurations:Debug-AS" }	
-		runtime "Debug"
-		symbols "on"
-		sanitize { "Address" }
-		flags { "NoRuntimeChecks", "NoIncrementalLink" }
-
-	filter "configurations:Release"
-		runtime "Release"
-		optimize "speed"
-
-    filter "configurations:Dist"
-		runtime "Release"
-		optimize "speed"
-        symbols "off"
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
