1de22b3890c6997e44350dda6848190d18b8a077
Pier-Olivier Boulianne <sheazywi@starbounded.me>
2025-04-09 19:13:30 -0400

    make it look readable

--- StarEngine/src/StarEngine/Scripting/ScriptEngine.cpp
+++ StarEngine/src/StarEngine/Scripting/ScriptEngine.cpp
@@ -10,8 +10,8 @@
 
 namespace StarEngine {
 
-	static std::unordered_map<std::string, ScriptFieldType> s_ScriptFieldTypeMap = {
-
+	static std::unordered_map<std::string, ScriptFieldType> s_ScriptFieldTypeMap =
+	{
 		{ "System.Single", ScriptFieldType::Float },
 		{ "System.Double", ScriptFieldType::Double },
 		{ "System.Boolean", ScriptFieldType::Bool },
@@ -105,8 +105,9 @@ namespace StarEngine {
 			}
 		}
 
-		ScriptFieldType MonoTypeToScriptFieldType(MonoType* type) {
-			std::string typeName = mono_type_get_name(type);
+		ScriptFieldType MonoTypeToScriptFieldType(MonoType* monoType)
+		{
+			std::string typeName = mono_type_get_name(monoType);
 
 			auto it = s_ScriptFieldTypeMap.find(typeName);
 			if (it == s_ScriptFieldTypeMap.end()) {
@@ -117,27 +118,30 @@ namespace StarEngine {
 			return it->second;
 		}
 
-		const char* ScriptFieldTypeToString(ScriptFieldType type) {
-			switch (type) {
-				case ScriptFieldType::Float:   return "Float";
-				case ScriptFieldType::Double:  return "Double";
-				case ScriptFieldType::Bool:    return "Bool";
-				case ScriptFieldType::Char:    return "Char";
-				case ScriptFieldType::Byte:    return "Byte";
-				case ScriptFieldType::Short:   return "Short";
-				case ScriptFieldType::Int:     return "Int";
-				case ScriptFieldType::Long:    return "Long";
-				case ScriptFieldType::UByte:   return "UByte";
-				case ScriptFieldType::UShort:  return "UShort";
-				case ScriptFieldType::UInt:    return "UInt";
-				case ScriptFieldType::ULong:   return "ULong";
-				case ScriptFieldType::Vector2: return "Vector2";
-				case ScriptFieldType::Vector3: return "Vector3";
-				case ScriptFieldType::Vector4: return "Vector4";
-				case ScriptFieldType::Entity:  return "Entity";
+		const char* ScriptFieldTypeToString(ScriptFieldType type)
+		{
+			switch (type)
+			{
+			case ScriptFieldType::Float:   return "Float";
+			case ScriptFieldType::Double:  return "Double";
+			case ScriptFieldType::Bool:    return "Bool";
+			case ScriptFieldType::Char:    return "Char";
+			case ScriptFieldType::Byte:    return "Byte";
+			case ScriptFieldType::Short:   return "Short";
+			case ScriptFieldType::Int:     return "Int";
+			case ScriptFieldType::Long:    return "Long";
+			case ScriptFieldType::UByte:   return "UByte";
+			case ScriptFieldType::UShort:  return "UShort";
+			case ScriptFieldType::UInt:    return "UInt";
+			case ScriptFieldType::ULong:   return "ULong";
+			case ScriptFieldType::Vector2: return "Vector2";
+			case ScriptFieldType::Vector3: return "Vector3";
+			case ScriptFieldType::Vector4: return "Vector4";
+			case ScriptFieldType::Entity:  return "Entity";
 			}
 			return "<Invalid>";
 		}
+
 	}
 
 	struct ScriptEngineData
@@ -373,11 +377,13 @@ namespace StarEngine {
 					scriptClass->m_Fields[fieldName] = { fieldType, fieldName, field };
 				}
 			}
+
 		}
 
 		auto& entityClasses = s_Data->EntityClasses;
 
 		//mono_field_get_value()
+
 	}
 
 	MonoImage* ScriptEngine::GetCoreAssemblyImage()
