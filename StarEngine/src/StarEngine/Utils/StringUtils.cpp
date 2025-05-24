1d6bc2926f2d15c7cbc43f721d8e61ecb0d24202
Pier-Olivier Boulianne <sheazywi@starbounded.me>
2025-05-24 13:40:27 -0400

    Added bytes to string util function

---
+++ StarEngine/src/StarEngine/Utils/StringUtils.cpp
@@ -0,0 +1,25 @@
+#include "sepch.h"
+#include "StringUtils.h"
+
+namespace StarEngine::Utils
+{
+	std::string BytesToString(uint64_t bytes)
+	{
+		constexpr uint64_t GB = 1024 * 1024 * 1024;
+		constexpr uint64_t MB = 1024 * 1024;
+		constexpr uint64_t KB = 1024;
+
+		char buffer[32 + 1]{};
+
+		if (bytes > GB)
+			snprintf(buffer, 32, "%.2f GB", (float)bytes / (float)GB);
+		else if (bytes > MB)
+			snprintf(buffer, 32, "%.2f MB", (float)bytes / (float)MB);
+		else if (bytes > KB)
+			snprintf(buffer, 32, "%.2f KB", (float)bytes / (float)KB);
+		else
+			snprintf(buffer, 32, "%.2f bytes", bytes);
+
+		return std::string(buffer);
+	}
+}
---
+++ StarEngine/src/StarEngine/Utils/StringUtils.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <string>
+
+namespace StarEngine::Utils {
+
+	std::string BytesToString(uint64_t bytes);
+
+}
